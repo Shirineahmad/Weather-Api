f2a54cf8481222924673772f3a566f31cded8eff
Shirineahmad <shirine.ahmad@hotmail.com>
2023-09-09 14:04:09 +0300

    merge with final3

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-=======
 import clear from './clear.svg';
 import cloudy from './cloudy.svg';
 import drizzle from './drizzle.svg';
@@ -159,7 +136,6 @@ fetchWeatherData();
     </div>
     </div>
     
->>>>>>> final
   );
 }
 
